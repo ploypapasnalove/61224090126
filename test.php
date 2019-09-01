6b6e72784e16059ec40960410590f96f9428e2f8
ploypapasnalove <54498334+ploypapasnalove@users.noreply.github.com>
2019-09-01 16:15:04 +0700

    โปรมแก้ไขฟอร์ม สร้างไฟล์ cookie.php
    cookie
    and option
    extares
    radio

--- test.php
+++ test.php
@@ -1,7 +1,12 @@
 <?php 
  
+ $type = $_POST["gender"]; 
+    echo $type;
+    echo "<br>";
     echo $_POST["name"]; 
     $fullname = $_POST["name"];
+    echo "<br/>";
+    echo $_POST["province"]; 
     echo "<br/>";
     echo $_POST["myfcolor"]; 
     $mfc = $_POST["myfcolor"]; 
@@ -11,8 +16,10 @@
     echo "<br/>";
     echo $_POST["pwd"];
     echo "<br/>";
-
+    echo $_POST["intro"];
+    echo "<br/>";
     echo "<font color=\"$mfc\" size=\"$size\">$fullname</font>";
-	
- 
+
+	Setcookie('city',$_POST["province"], time()+60);
+    echo "<a href=\"cookie.php\">ดูค่า Cookie</a>";
 ?>
